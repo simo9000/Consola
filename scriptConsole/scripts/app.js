c0c22754a459f651ba619f4ba87c3b7be1770372
simo9000 <alkylate2000@gmail.com>
2016-03-24 21:33:13 -0700

    Made commandLine and commandPrompt views with nesting

--- scriptConsole/scripts/app.js
+++ scriptConsole/scripts/app.js
@@ -2,29 +2,79 @@
 /// <reference path="~/scripts/linq-vsdoc.js"/>
 /// <reference path="~/scripts/jquery-2.2.1.js"/>
 /// <refernece path="~/scripts/jquery-2.1.0-vsdoc.js"/>
+var ENTER_KEY = 13;
 var commandLine = Backbone.View.extend({
 
-    tagName: 'li',
+    tagName: 'tr',
 
     initialize: function() {
-        
+        this.prompt = "$>";
     },
 
-    render: function (opts) {
+    events: {
+        "keyup .command": "CRLF"
+    },
+
+    CRLF: function(e){
         var view = this;
-        $.get('/templates/emptyCmdLine.html', function (template) {
-            var renderedView = Mustache.render(template, {
-                prompt: "$>"
+        if (e.which === ENTER_KEY) {
+            var line = this.$el.find('.command').val();
+            $.get('/templates/finishedCmdLine.html', function (template) {
+                var renderedLine = Mustache.render(template, {
+                    prompt: view.prompt,
+                    content: line
+                });
+                view.$el.html(renderedLine);
+                view.$el.find('.command').focus();
+                view.trigger('newLine');
             });
-            view.$el.html(renderedView);
-        });
+        }
+    },
+
+    render: function () {
+        var view = this;
+        if (this.template == undefined)
+            $.get('/templates/emptyCmdLine.html', function (template) {
+                this.emptyTemplate = template;
+                var renderedView = Mustache.render(template, {
+                    prompt: view.prompt
+                });
+                view.$el.html(renderedView);
+                view.$el.find('.command').focus();
+            });
+        else
         return this;
+    }
+});
+
+var commandPrompt = Backbone.View.extend({
+
+    tagName: 'tbody',
+
+    createNewLine: function() {
+        this.activeCommand = new commandLine({});
+        this.listenTo(this.activeCommand, 'newLine', this.addNewLine);
+    },
+
+    initialize: function () {
+        this.createNewLine();
+    },
+
+    render: function (opt) {
+        this.activeCommand.render();
+        this.$el.html(this.activeCommand.el);
+    },
+
+    addNewLine: function () {
+        this.createNewLine();
+        this.activeCommand.render();
+        this.$el.append(this.activeCommand.el);
     }
 });
 
 
 $(document).ready(function () {
-    var line = new commandLine({});
-    line.render();
-    $('#console').append(line.el);
+    var prompt = new commandPrompt({});
+    prompt.render();
+    $('#console').append(prompt.el);
 });
